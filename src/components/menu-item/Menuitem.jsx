d5e5750d535c6b6dd683b8f75dffec043c32e872
yusufyi@hotmail.com <yusufyi@hotmail.com>
2021-03-15 19:28:05 +0000

    template of page before router

---
+++ src/components/menu-item/Menuitem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './menuitem.scss';
+const Menuitem = ({ title, imageUrl, size }) => {
+  console.log(imageUrl);
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default Menuitem;
